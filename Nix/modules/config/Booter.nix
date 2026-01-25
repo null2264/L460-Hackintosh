cf881cac34b53232f166f5c24d5c6b4e1da56088
Ahmad Ansori Palembani <palembani@gmail.com>
2026-01-25 17:24:12 +0700

    fix: Board-ID patch

--- Nix/modules/config/Booter.nix
+++ Nix/modules/config/Booter.nix
@@ -1,9 +1,24 @@
 { lib, pkgs, ... }:
 
+with lib.oc.plist;
 {
   oceanix.opencore.settings.Booter = {
     MmioWhitelist = [];
-    Patch = [];
+    Patch = [
+      {
+        Arch = "x86_64";
+        Comment = "Skip Board ID check";
+        Count = 0;
+        Enabled = true;
+        Find = mkData "AFAAbABhAHQAZgBvAHIAbQBTAHUAcABwAG8AcgB0AC4AcABsAGkAcwB0";
+        Identifir = "Apple";
+        Limit = 0;
+        Mask = mkData "";
+        Replace = mkData "AC4ALgAuAC4ALgAuAC4ALgAuAC4ALgAuAC4ALgAuAC4ALgAuAC4ALgAu";
+        ReplaceMask = mkData "";
+        Skip = 0;
+      }
+    ];
 
     Quirks = {
       FixupAppleEfiImages = false;
